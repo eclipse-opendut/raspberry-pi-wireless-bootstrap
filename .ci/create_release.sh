299966bf6406260cbed362cadac2e9b9193096ca
Frank Märkle <Frank.Maerkle@mercedes-benz.com>
2025-06-27 11:59:50 +0200

    CI -> Implement building macOS distribution (only on macOS).

--- .ci/create_release.sh
+++ .ci/create_release.sh
@@ -1,21 +1,40 @@
 #!/bin/sh
 
+set -e
+
 version=$(cargo metadata --format-version=1 | jq --raw-output '.packages[] | select(.name | contains("raspberry_pi_wireless_bootstrap")) | .version')
 
+targets="x86_64-apple-darwin x86_64-unknown-linux-gnu"
 
-distribution_dir=target/distribution
-rm -r $distribution_dir
-mkdir --parents $distribution_dir
+for target in $targets; do
 
-out_path=$distribution_dir/raspberry_pi_wireless_bootstrap_linux_x86_64_${version}.tar.gz
+    if [ $target != "x86_64-apple-darwin" ]; then
+        cross build --release --target=$target
+    else
+        host_cpu_arch=$(rustc -vV | grep "host:")
 
-cross build --release --target=x86_64-unknown-linux-gnu
+        if [ "$host_cpu_arch" = "host: x86_64-apple-darwin" ]; then
+            cargo build --release
+        else
+            echo "Skipping build for macOS, since we are not on a macOS system. Cross-compiling for macOS is generally not supported/allowed by Apple."
+            continue;
+        fi
+    fi
 
-tar --create --gzip \
-    --file=$out_path \
-    --directory=target/x86_64-unknown-linux-gnu/release/ \
-    raspberry_pi_wireless_bootstrap \
-    ../../../README.md
 
-echo "Distribution has been built underneath:\n\n  $out_path"
-echo "\nNow you need to manually upload it to a GitHub release."
+    distribution_dir=target/distribution
+    rm -r $distribution_dir
+    mkdir --parents $distribution_dir
+
+    out_path=$distribution_dir/raspberry_pi_wireless_bootstrap_${target}_${version}.tar.gz
+
+    tar --create --gzip \
+        --file=$out_path \
+        --directory=target/$target/release/ \
+        raspberry_pi_wireless_bootstrap \
+        ../../../README.md
+
+    echo "Distribution has been built underneath:\n\n  $out_path\n"
+done
+
+echo "Now you need to manually upload the distributions to a GitHub release."
